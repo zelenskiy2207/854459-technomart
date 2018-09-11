372764033b13fc6d280c8801f3a942e3e584960e
Sergey Zelenskiy <zelenskiy1104@gmail.com>
2018-09-11 13:50:38 +0600

    правки после первой проверки

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ if (close) {
     close[i].addEventListener("click", function (e) {
       e.preventDefault();
       var popup = this.parentNode;
-      popup.classList.remove('visible');
+      popup.classList.remove("visible");
+      popup.classList.remove("modal-error");
     });
   }
 }
@@ -14,17 +15,73 @@ if (map_opener) {
   map_opener.addEventListener("click", function (e) {
     e.preventDefault();
     var popup = document.querySelector(".modal-map");
-    popup.classList.add('visible');
+    popup.classList.add("visible");
   });
+}
+
+var isStorageSupport = true;
+var username_stored = "";
+var email_stored = "";
+try {
+  username_stored = localStorage.getItem("username");
+  email_stored = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
 }
 
 var write_us_opener = document.querySelector(".write-us");
 if (write_us_opener) {
+
+  var write_us_modal = document.querySelector(".modal-write-us");
+  var username = write_us_modal.querySelector("[name=name]");
+  var email = write_us_modal.querySelector("[name=email]");
+  var message_text = write_us_modal.querySelector("[name=message-text]");
+  var form = write_us_modal.querySelector("form");
+
   write_us_opener.addEventListener("click", function (e) {
     e.preventDefault();
-    var popup = document.querySelector(".modal-write-us");
-    popup.classList.add('visible');
+    write_us_modal.classList.add("visible");
+
+    if (username_stored || email_stored) {
+      if (username_stored) {
+        username.value = username_stored;
+        email.focus();
+      }
+      if (email_stored) {
+        email.value = email_stored;
+        message_text.focus();
+      }
+    }
+    else {
+      username.focus();
+    }
   });
+
+  form.addEventListener("submit", function (evt) {
+    if (!username.value || !email.value || !message_text.value) {
+      evt.preventDefault();
+      write_us_modal.classList.remove("modal-error");
+      write_us_modal.offsetWidth = write_us_modal.offsetWidth;
+      write_us_modal.classList.add("modal-error");
+      console.log("Нужно заполнить все поля");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("email", email.value);
+      }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (write_us_modal.classList.contains("visible")) {
+        write_us_modal.classList.remove("visible");
+        write_us_modal.classList.remove("modal-error");
+      }
+    }
+  });
+
 }
 
 var buy_openers = document.querySelectorAll(".catalog-item-buy");
@@ -33,7 +90,7 @@ if (buy_openers) {
     buy_openers[i].addEventListener("click", function (e) {
       e.preventDefault();
       var popup = document.querySelector(".modal-added-to-cart");
-      popup.classList.add('visible');
+      popup.classList.add("visible");
     });
   }
 }
